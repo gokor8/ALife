e8d9ab38c792f3f5b71a1a848f2940ce3a1e4747
grigory.paravyan <g.bagramyan@growapp.biz>
2023-03-01 10:41:56 +0300

    Add usernmae dog mask. Need tests

--- app/src/main/java/com/alife/anotherlife/core/composable/text/text_formation/mask/units/StaticUnits.kt
+++ app/src/main/java/com/alife/anotherlife/core/composable/text/text_formation/mask/units/StaticUnits.kt
@@ -3,13 +3,15 @@ package com.alife.anotherlife.core.composable.text.text_formation.mask.units
 import com.alife.anotherlife.core.composable.text.text_formation.mask.MaskUnit
 
 
-sealed class StaticUnits(symbol: Char) : MaskUnit.SymbolMaskUnit(symbol) {
+abstract class StaticUnits(symbol: Char) : MaskUnit.SymbolMaskUnit(symbol) {
 
     class Space : StaticUnits(' ')
 
     class RusCode : StaticUnits('7')
 
     class Dot : StaticUnits('.')
+
+    class Email : StaticUnits('@')
 
     class Separator : StaticUnits('-')
 
